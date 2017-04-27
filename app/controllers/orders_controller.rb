ceee9b403847e1fc738f09884e43a88e3d224dc2
Kristin <kvcmorin@gmail.com>
2017-04-27 23:07:46 +0000

    now a user cannot checkout their cart if they are not signed in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
 
   def create_order(stripe_charge)
     order = Order.new(
-      email: params[:stripeEmail],
+      email: current_user.email,
       total_cents: cart_total,
       stripe_charge_id: stripe_charge.id, # returned by stripe
     )
