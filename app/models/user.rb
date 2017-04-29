a4aece257c99882df8da6642caea5d817597ac8f
Kristin <kvcmorin@gmail.com>
2017-04-29 02:39:29 +0000

    added email uniqueness and cleaned up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 	validates :password_digest, presence: true
 
 end
